f87ff01256fe50c6e5b65ee48ba2910a5b10e1d9
VictorzllDev <VictorzllDev@gmail.com>
2025-02-06 13:54:03 -0300

    feat: Create login page (/login)
    - Add form with email and password fields.
    - Integrate with the POST /auth/login endpoint.

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router'
 import { Home } from './pages/Home'
 import { Register } from './pages/Register'
+import { Login } from './pages/Login'
 
 export const router = createBrowserRouter([
 	{
@@ -13,6 +14,10 @@ export const router = createBrowserRouter([
 			{
 				path: '/register',
 				element: <Register />,
+			},
+			{
+				path: '/login',
+				element: <Login />,
 			},
 		],
 	},
--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -1,16 +1,30 @@
 import { apiClientService } from './api-client.service'
 
 // Create a new user - register
-interface IRegisterAuth {
+interface IRegisterRequest {
 	email: string
 	password: string
 }
-export async function registerAuth({ email, password }: IRegisterAuth) {
-	await apiClientService.post<void>('/auth/register', {
+export async function registerAuth({ email, password }: IRegisterRequest) {
+	await apiClientService.post('/auth/register', {
 		email,
 		password,
 	})
 }
 
 // Login user - Login
-export function loginAuth() {}
+interface ILoginRequest {
+	email: string
+	password: string
+}
+interface ILoginResponse {
+	token: string
+}
+export async function loginAuth({ email, password }: ILoginRequest) {
+	const result = await apiClientService.post<ILoginResponse>('/auth/login', {
+		email,
+		password,
+	})
+
+	return result.data
+}
