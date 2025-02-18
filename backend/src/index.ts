8677179b19a704f3b9b6e859e73f29674710b742
VictorzllDev <VictorzllDev@gmail.com>
2025-02-18 18:46:48 -0300

    add zod type provider, router auth

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,39 @@
-import fastifyCors from '@fastify/cors'
-import fastify from 'fastify'
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
 import { env } from './env'
 import { authRoutes } from './routes/auth.routes'
 import { productRoutes } from './routes/product.routes'
 import { prisma } from './utils/prisma-client.util'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
 app.register(fastifyCors, {
 	origin: true,
+})
+
+app.register(fastifySwagger, {
+	openapi: {
+		info: {
+			title: 'Budzapp API',
+			version: '0.0.0',
+		},
+	},
+	transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+	routePrefix: '/docs',
 })
 
 app.register(authRoutes, {
--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,57 +1,131 @@
-import type { FastifyInstance } from 'fastify'
-import z from 'zod'
+import { z, ZodError } from 'zod'
 import { AuthRepository } from '../repositories/auth.repository'
 import type { IAuthRequest } from '../types/auth.types'
+import type { FastifyTypeInstace } from '../types/fastify.types'
 import { AuthUseCase } from '../usecases/auth.usecase'
 
 const authUseCase = new AuthUseCase(new AuthRepository())
 
-export function authRoutes(app: FastifyInstance) {
-	app.post<{ Body: IAuthRequest }>('/register', async (req, reply) => {
-		try {
-			const bodySchema = z.object({
-				email: z.string().email(),
-				password: z.string().min(6).max(32),
-			})
-			const { email, password } = bodySchema.parse(req.body)
+export function authRoutes(app: FastifyTypeInstace) {
+	app.post(
+		'/register',
+		{
+			schema: {
+				tags: ['auth'],
+				description: 'Register new user',
+				body: z.object({
+					email: z.string().email(),
+					password: z.string().min(6).max(32),
+				}),
+				response: {
+					201: z.void(),
+					500: z.unknown(),
+				},
+			},
+		},
+		async (req, reply) => {
+			try {
+				const { email, password } = req.body
 
-			const result = await authUseCase.register({ email, password })
-			reply.status(201).send(result)
-		} catch (error) {
-			console.log(error)
-			reply.status(500).send(error)
-		}
-	})
+				const result = await authUseCase.register({ email, password })
+				reply.status(201).send(result)
+			} catch (error) {
+				if (error instanceof ZodError) {
+					reply.status(400).send({
+						message: 'Validation failed',
+						errors: error.errors.map((e) => ({
+							message: e.message,
+							path: e.path,
+						})),
+					})
+				} else {
+					console.log(error)
+					reply.status(500).send(error)
+				}
+			}
+		},
+	)
 
-	app.post<{ Body: IAuthRequest }>('/login', async (req, reply) => {
-		try {
-			const bodySchema = z.object({
-				email: z.string().email(),
-				password: z.string().min(6).max(32),
-			})
-			const { email, password } = bodySchema.parse(req.body)
+	app.post(
+		'/login',
+		{
+			schema: {
+				tags: ['auth'],
+				description: 'Login user',
+				body: z.object({
+					email: z.string().email(),
+					password: z.string().min(6).max(32),
+				}),
+				response: {
+					200: z.object({
+						token: z.string(),
+					}),
+					500: z.unknown(),
+				},
+			},
+		},
+		async (req, reply) => {
+			try {
+				const { email, password } = req.body
 
-			const result = await authUseCase.login({ email, password })
-			reply.status(200).send(result)
-		} catch (error) {
-			console.log(error)
-			reply.status(500).send(error)
-		}
-	})
+				const result = await authUseCase.login({ email, password })
+				reply.status(200).send(result)
+			} catch (error) {
+				if (error instanceof ZodError) {
+					reply.status(400).send({
+						message: 'Validation failed',
+						errors: error.errors.map((e) => ({
+							message: e.message,
+							path: e.path,
+						})),
+					})
+				} else {
+					console.log(error)
+					reply.status(500).send(error)
+				}
+			}
+		},
+	)
 
-	app.get('/validate', async (req, reply) => {
-		try {
-			const headersSchema = z.object({
-				authorization: z.string(),
-			})
-			const { authorization } = headersSchema.parse(req.headers)
-			const token = authorization.split(' ')[1]
+	app.get(
+		'/validate',
+		{
+			schema: {
+				tags: ['auth'],
+				description: 'Validate token',
+				headers: z.object({
+					authorization: z.string(),
+				}),
+				response: {
+					200: z.void(),
+					500: z.unknown(),
+				},
+			},
+		},
+		async (req, reply) => {
+			try {
+				const headersSchema = z.object({
+					authorization: z.string(),
+				})
+				const { authorization } = headersSchema.parse(req.headers)
+				const token = authorization.split(' ')[1]
 
-			const result = await authUseCase.validateToken(token)
-			reply.status(200).send(result)
-		} catch (error) {
-			console.log(error)
-			reply.status(500).send(error)
-		}
-	})
+				const result = await authUseCase.validateToken(token)
+				reply.status(200).send(result)
+			} catch (error) {
+				if (error instanceof ZodError) {
+					reply.status(400).send({
+						message: 'Validation failed',
+						errors: error.errors.map((e) => ({
+							message: e.message,
+							path: e.path,
+						})),
+					})
+				} else {
+					console.log(error)
+					reply.status(500).send(error)
+				}
+			}
+		},
+	)
 }
